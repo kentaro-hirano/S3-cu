e3d2f359cf8cdc229a589906eabdc220a2dfd5cf
EC2 Default User <ec2-user@ip-172-31-50-175.ec2.internal>
2021-02-18 08:19:06 +0000

    create ajax favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,14 @@
 class FavoritesController < ApplicationController
 
   def create
-    travel = Travel.find(params[:travel_id])
-    favorite = current_user.favorites.new(travel_id: travel.id)
+    @travel = Travel.find(params[:travel_id])
+    favorite = current_user.favorites.new(travel_id: @travel.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    favorite = Favorite.find_by(travel_id: params[:travel_id])
+    @travel = Travel.find(params[:travel_id])
+    favorite = Favorite.find_by(travel_id: @travel.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
