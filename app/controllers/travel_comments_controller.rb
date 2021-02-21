d5f0929d7651e6fb39aaf4868bb7b18d171b6d01
EC2 Default User <yakyukentaro@gmail.com>
2021-02-21 04:27:31 +0000

    create comments ajax

--- app/controllers/travel_comments_controller.rb
+++ app/controllers/travel_comments_controller.rb
@@ -1,18 +1,17 @@
 class TravelCommentsController < ApplicationController
   
   def create
-    travel = Travel.find(params[:travel_id])
+    @travel = Travel.find(params[:travel_id])
     comment = TravelComment.new(travel_comment_params)
     comment.user_id = current_user.id
-    comment.travel_id = travel.id
+    comment.travel_id = @travel.id
     comment.save
-    redirect_to travel_path(travel)
   end
   
   def destroy
-    comment = TravelComment.find_by(travel_id: params[:travel_id])
+    @travel = Travel.find(params[:travel_id])
+    comment = TravelComment.find_by(travel_id: @travel.id)
     comment.destroy
-    redirect_to travel_path(params[:travel_id])
   end
   
   private
